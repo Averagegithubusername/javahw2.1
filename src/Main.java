52cfdd9ec4a795541b47c128816c115fd79444f0
Averagegithubusername <s0mething_different@mail.ru>
2021-12-02 02:39:44 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float price = (float) 15999.2;
+        int count = 20;
+        int miles = (int) (price / count);
+        System.out.println(miles + " миль начислено");
+    }
+}
